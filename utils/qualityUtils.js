31995b53db28bfc7b9e9c049939563f416a8eae1
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-15 10:31:46 -0700

    Update AQI notes

--- utils/qualityUtils.js
+++ utils/qualityUtils.js
@@ -1,14 +1,15 @@
 const QUALITY = ['Good', 'Moderate', 'Unhealthy for Sensitive Groups', 'Unhealthy', 'Very Unhealthy', 'Hazardous', 'Very Hazardous']
 const QUALITY_CLASSES = ['good', 'moderate', 'unhealthySensitive', 'unhealthy', 'veryUnhealthy', 'hazardous', 'veryHazardous']
 
+// UPDATED 2024
 /*                                       AQI     RAW PM2.5
-	Good                                0 - 50  |  0.0 – 12.0
-	Moderate                          51 - 100  |  12.1 – 35.4
+	Good                                0 - 50  |  0.0 – 9.0
+	Moderate                          51 - 100  |  9.1 – 35.4
 	Unhealthy for Sensitive Groups   101 – 150  |  35.5 – 55.4
-	Unhealthy                        151 – 200  |  55.5 – 150.4
-	Very Unhealthy                   201 – 300  |  150.5 – 250.4
-	Hazardous                        301 – 400  |  250.5 – 350.4
-	Very Hazardous                   401 – 500  |  350.5 – 500.4
+	Unhealthy                        151 – 200  |  55.5 – 125.4
+	Very Unhealthy                   201 – 300  |  125.5 – 225.4
+	Hazardous                        301 – 400  |  225.5 – 325.4
+	Very Hazardous                   401 – 500  |  325.5 – 500.4
 */
 
 export const getQuality = aqi => {
